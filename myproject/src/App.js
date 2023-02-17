23f0bfeeb6aed25b605aecb0cb91ef87b5aa83bc
Telicia Watson <teli203@aol.com>
2023-02-17 10:08:29 -0600

    added more to the title link

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more about React with me 
         </a>
       </header>
     </div>
