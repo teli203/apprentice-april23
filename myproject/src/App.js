3c19f306ca1209317ad2b6a7338124d3b917f521
Telicia Watson <teli203@aol.com>
2023-02-17 11:54:16 -0600

    utilizing the component with html tag

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Greeting from './components/Greeting'
 function App() {
   return (
     <div className="App">
+      <Greeting></Greeting>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
