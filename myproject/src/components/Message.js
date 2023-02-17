ff01a7507bd16f105e9a65b720376b22393501aa
Telicia Watson <teli203@aol.com>
2023-02-17 13:20:59 -0600

    make sentence flow w/ rest of the pg

--- myproject/src/components/Message.js
+++ myproject/src/components/Message.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 class Message extends Component {
     render() {
-        return <h1>This is an example of a class component</h1>;
+        return <h1>Program from April 24, 2023 - November 3rd, 2023. </h1>;
     }
 }
 
